64fc00ffd3b90abcf7780b495e4c4d88449dd129
RezalMaksmi <rezalnurscc@gmail.com>
2024-09-21 01:34:36 +0700

    merapikan tampilan homepeserta & profile peserta

--- src/pages/peserta/HomePeserta.jsx
+++ src/pages/peserta/HomePeserta.jsx
@@ -42,111 +42,121 @@ const HomePeserta = () => {
     fetchDataTest();
   }, [token_user]);
   return (
-    <div className="w-full h-auto flex flex-col justify-center">
-      <div className="mt-[75px] grid grid-cols-8 gap-10 lg:px-[220px] w-full">
-        <div className="bg-[#E4EDF1] col-span-6 flex items-center justify-between mt-10 rounded-3xl shadow-lg p-10">
-          <div className="flex-col text-gray-800">
-            <p className="font-bold text-6xl">Hello, {resTest.nama_peserta}</p>
-            <p className="font-semibold text-2xl mt-3">
-              Selamat Datang di Brawijaya Language Center
+    <div className="bg-[#f8f8f8]">
+      <div className="max-w-[1800px] mx-auto px-10 w-full h-auto flex flex-col justify-center">
+        <div className=" mt-[75px] grid grid-cols-8 gap-9 w-full">
+          <div className="bg-[#E4EDF1] col-span-6 flex items-center justify-between mt-10 rounded-3xl shadow-lg p-10">
+            <div className="flex-col text-gray-800">
+              <p className="font-bold text-6xl">
+                Hello, {resTest.nama_peserta}
+              </p>
+              <p className="font-semibold text-2xl mt-3">
+                Selamat Datang di Brawijaya Language Center
+              </p>
+            </div>
+            <Images src={book_main} />
+          </div>
+          <div className="bg-gray-50 h-auto col-span-2 flex justify-center items-center flex-col mt-10 rounded-3xl shadow-lg p-10 ring-1 ring-[#dddddd]">
+            <Images src={user_profile} className="h-[200px] " />
+            <p className="font-semibold text-center text-2xl text-gray-900 mb-3">
+              {resTest.nama_peserta}
             </p>
-          </div>
-          <Images src={book_main} />
-        </div>
-        <div className="bg-gray-50 h-auto col-span-2 flex justify-center items-center flex-col mt-10 rounded-3xl shadow-lg p-10 ring-1 ring-gray-500">
-          <Images src={user_profile} className="h-[200px] " />
-          <p className="font-semibold text-center text-2xl text-gray-900 mb-3">
-            {resTest.nama_peserta}
-          </p>
-          <div className="flex gap-4">
-            <Link to="/profile-peserta">
-              <button className="bg-blue-500  text-gray-50 py-2 px-6 rounded-md font-semibold">
-                Lihat profil
+            <div className="flex gap-4">
+              <Link to="/profile-peserta">
+                <button className="bg-blue-500  text-gray-50 py-2 px-6 rounded-md font-semibold">
+                  Lihat profil
+                </button>
+              </Link>
+              <button
+                onClick={(e) => handleLogout(e)}
+                className="bg-red-500  text-gray-50 py-2 px-6 rounded-md font-semibold"
+              >
+                Logout
               </button>
-            </Link>
-            <button
-              onClick={(e) => handleLogout(e)}
-              className="bg-red-500  text-gray-50 py-2 px-6 rounded-md font-semibold"
-            >
-              Logout
-            </button>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="lg:px-[220px] mt-10 grid grid-cols-6">
-        <div className="bg-gray-50 col-span-4 rounded-3xl shadow-lg px-5 py-3">
-          <p className="font-semibold text-gray-800 text-2xl">
-            Test yang anda ikuti
-          </p>
-          <div className="grid grid-cols-4 gap-6 my-5">
-            {test.map((item, index) => (
-              <div key={index}>
-                {item.id == resTest.id_test ? (
-                  <Link to={`/test-peserta/${item.id}`}>
-                    <div className="bg-blue-100 cursor-pointer flex justify-center items-center flex-col rounded-3xl shadow-lg p-5 ring-1 ring-gray-500">
-                      <Images src={book_active} className="h-[150px] " />
+        <div className=" mt-10 grid grid-cols-6">
+          <div className="bg-white col-span-4 rounded-3xl shadow-sm border border-[#f0f0f0] px-5 py-3">
+            <p className="font-semibold text-gray-800 text-2xl">
+              Test yang anda ikuti
+            </p>
+            <div className="grid grid-cols-4 gap-6 my-5">
+              {test.map((item, index) => (
+                <div key={index}>
+                  {item.id == resTest.id_test ? (
+                    <Link to={`/test-peserta/${item.id}`}>
+                      <div className="bg-blue-100 cursor-pointer flex justify-center items-center flex-col rounded-3xl shadow-lg p-5 ring-1 ring-gray-500">
+                        <Images src={book_active} className="h-[150px] " />
+                        <p className="font-semibold text-center text-xl mt-2">
+                          {item.jenis_test}
+                        </p>
+                      </div>
+                    </Link>
+                  ) : (
+                    <div className="bg-white cursor-not-allowed flex justify-center items-center flex-col rounded-3xl shadow-lg p-5 ring-1 ring-gray-500">
+                      <Images src={book_deactive} className="h-[150px] " />
                       <p className="font-semibold text-center text-xl mt-2">
                         {item.jenis_test}
                       </p>
                     </div>
-                  </Link>
-                ) : (
-                  <div className="bg-blue-100 cursor-not-allowed flex justify-center items-center flex-col rounded-3xl shadow-lg p-5 ring-1 ring-gray-500">
-                    <Images src={book_deactive} className="h-[150px] " />
-                    <p className="font-semibold text-center text-xl mt-2">
-                      {item.jenis_test}
-                    </p>
-                  </div>
-                )}
-              </div>
-            ))}
+                  )}
+                </div>
+              ))}
+            </div>
           </div>
         </div>
-      </div>
-      <div className="grid grid-cols-6 lg:px-[220px] my-10">
-        <div className=" col-span-4 rounded-3xl shadow-xl px-5 py-3">
-          <p className="font-semibold text-gray-800 text-2xl mb-4">
-            Nilai Hasil Test
-          </p>
-          <div className="bg-[#E4EDF1] rounded-3xl px-3 py-2">
-            <div className="grid grid-cols-5 gap-3 px-3 py-2">
-              <div className="bg-blue-500 rounded-xl px-5 py-6 h-full flex items-center justify-center">
-                <p className="font-semibold text-3xl text-center text-gray-50">
-                  Pretest
-                </p>
-              </div>
-              <div className="col-span-3 bg-blue-500 grid grid-cols-3 gap-3 rounded-xl p-4">
-                <div className="bg-gray-50 rounded-xl px-3 py-2">
-                  <p className="font-semibold text-blue-500 text-center">
-                    Listening
-                  </p>
-                  <p className="font-semibold text-5xl text-center my-4">
-                    {resTest.listening == null ? "-" : (resTest.listening+'/'+resTest.k_listening)}
+        <div className="grid grid-cols-6  my-10">
+          <div className="bg-white col-span-4 rounded-3xl shadow-sm border border-[#f0f0f0] px-5 py-3">
+            <p className="font-semibold text-gray-800 text-2xl mb-4">
+              Nilai Hasil Test
+            </p>
+            <div className="bg-[#E4EDF1] rounded-3xl px-3 py-2">
+              <div className="grid grid-cols-5 gap-3 px-3 py-2">
+                <div className="bg-blue-500 rounded-xl px-5 py-6 h-full flex items-center justify-center">
+                  <p className="font-semibold text-3xl text-center text-gray-50">
+                    Pretest
                   </p>
                 </div>
-                <div className="bg-gray-50 rounded-xl px-3 py-2">
-                  <p className="font-semibold text-blue-500 text-center">
-                    Structure
-                  </p>
-                  <p className="font-semibold text-5xl text-center my-4">
-                    {resTest.structure == null ? "-" : (resTest.structure+'/'+resTest.k_structure)}
-                  </p>
+                <div className="col-span-3 bg-blue-500 grid grid-cols-3 gap-3 rounded-xl p-4">
+                  <div className="bg-gray-50 rounded-xl px-3 py-2">
+                    <p className="font-semibold text-blue-500 text-center">
+                      Listening
+                    </p>
+                    <p className="font-semibold text-5xl text-center my-4">
+                      {resTest.listening == null
+                        ? "-"
+                        : resTest.listening + "/" + resTest.k_listening}
+                    </p>
+                  </div>
+                  <div className="bg-gray-50 rounded-xl px-3 py-2">
+                    <p className="font-semibold text-blue-500 text-center">
+                      Structure
+                    </p>
+                    <p className="font-semibold text-5xl text-center my-4">
+                      {resTest.structure == null
+                        ? "-"
+                        : resTest.structure + "/" + resTest.k_structure}
+                    </p>
+                  </div>
+                  <div className="bg-gray-50 rounded-xl px-3 py-2">
+                    <p className="font-semibold text-blue-500 text-center">
+                      Reading
+                    </p>
+                    <p className="font-semibold text-5xl text-center my-4">
+                      {resTest.reading == null
+                        ? "-"
+                        : resTest.reading + "/" + resTest.k_reading}
+                    </p>
+                  </div>
                 </div>
-                <div className="bg-gray-50 rounded-xl px-3 py-2">
-                  <p className="font-semibold text-blue-500 text-center">
-                    Reading
-                  </p>
-                  <p className="font-semibold text-5xl text-center my-4">
-                    {resTest.reading == null ? "-" : (resTest.reading+'/'+resTest.k_reading)}
-                  </p>
-                </div>
-              </div>
-              <div className="bg-blue-950 rounded-xl p-2">
-                <p className="text-gray-50 font-semibold">Total</p>
-                <div className="rounded-xl bg-gray-50 py-6">
-                  <p className="font-semibold text-5xl text-center">
-                    {resTest.total == null ? "-" : resTest.total}
-                  </p>
+                <div className="bg-blue-950 rounded-xl p-2">
+                  <p className="text-gray-50 font-semibold">Total</p>
+                  <div className="rounded-xl bg-gray-50 py-6">
+                    <p className="font-semibold text-5xl text-center">
+                      {resTest.total == null ? "-" : resTest.total}
+                    </p>
+                  </div>
                 </div>
               </div>
             </div>
--- src/pages/peserta/ProfilePeserta.jsx
+++ src/pages/peserta/ProfilePeserta.jsx
@@ -1,107 +1,220 @@
 import axios from "axios";
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Button } from "../../components";
 import { useNavigate } from "react-router-dom";
 import user_profile from "../../assets/img/default-profile.png";
 import { Images } from "../../assets";
 
 const ProfilePeserta = () => {
-    const [user, setUser] = useState([]);
-    const navigate = useNavigate();
+  const [user, setUser] = useState([]);
+  const navigate = useNavigate();
 
-    const token = sessionStorage.getItem('peserta_id');
+  const token = sessionStorage.getItem("peserta_id");
 
-    const fetchUser = async () => {
-        const response = await axios.get(`http://localhost:8000/user/result/${token}`);
-        setUser(response.data.data);
-    }
-
-    useEffect(() =>{
-        fetchUser();
-    }, []);
-    
-    return(
-        <div className="w-full h-auto flex flex-col justify-center">
-            <div className="mt-[75px] gap-10 lg:px-[220px] w-full">
-                <div className="px-4 py-8 my-10 rounded-lg shadow-lg">
-                    <h2 className="text-gray-800 font-bold text-2xl mx-5 my-3">Profile Peserta </h2>
-                    <Images src={user_profile} className="h-[200px] " />
-                    <table className="font-semibold text-gray-800 mx-5">
-                        <tr className="pt-4">
-                            <td>
-                                <span className="text-xl">NIK/NIM</span>
-                            </td>
-                            <td className="pl-2">
-                                <span className="text-lg">: {user.no_reg}</span>
-                            </td>
-                        </tr>
-                        <tr className="pt-8">
-                            <td>
-                                <span className="text-xl">Nama Peserta</span>
-                            </td>
-                            <td className="pl-2">
-                                <span className="text-lg">: {user.nama_peserta}</span>
-                            </td>
-                        </tr>
-                        <tr className="pt-4">
-                            <td>
-                                <span className="text-xl">Email</span>
-                            </td>
-                            <td className="pl-2">
-                                <span className="text-lg">: {user.email}</span>
-                            </td>
-                        </tr>
-                        <tr className="pt-4">
-                            <td>
-                                <span className="text-xl">No. HP</span>
-                            </td>
-                            <td className="pl-2">
-                                <span className="text-lg">: {user.no_hp}</span>
-                            </td>
-                        </tr>
-                        <tr className="pt-4">
-                            <td>
-                                <span className="text-xl">Tgl. Lahir</span>
-                            </td>
-                            <td className="pl-2">
-                                <span className="text-lg">: {user.tgl_lahir}</span>
-                            </td>
-                        </tr>
-                        <tr className="pt-4">
-                            <td>
-                                <span className="text-xl">Jenis Kelamin</span>
-                            </td>
-                            <td className="pl-2">
-                                <span className="text-lg">: {user.gender}</span>
-                            </td>
-                        </tr>
-                        <tr className="pt-4">
-                            <td>
-                                <span className="text-xl">Instansi</span>
-                            </td>
-                            <td className="pl-2">
-                                <span className="text-lg">: {user.instansi}</span>
-                            </td>
-                        </tr>
-                        <tr className="pt-4">
-                            <td>
-                                <span className="text-xl">Alamat</span>
-                            </td>
-                            <td className="pl-2">
-                                <span className="text-lg">: {user.alamat}</span>
-                            </td>
-                        </tr>
-                    </table>
-                    <Button
-                        type={"PrimaryButton"}
-                        onClick={() => navigate('/dashboard-peserta')}
-                        text={"Kembali"}
-                        className="bg-[#1283b6] text-white my-10 mx-5"
-                    />
-                </div>
-            </div>
-        </div>
+  const fetchUser = async () => {
+    const response = await axios.get(
+      `http://localhost:8000/user/result/${token}`
     );
+    setUser(response.data.data);
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <div className="max-w-[1800px] w-full px-10 min-h-[91vh] h-auto flex flex-col ">
+      <div className="mt-[75px] gap-10  w-full">
+        <div className="grid grid-cols-3 w-full px-4 py-8 my-10 rounded-lg gap-7">
+          {/* <h2 className="text-gray-800 font-bold text-2xl mx-5 my-3">
+            Profile Peserta
+          </h2>
+          <div className="flex gap-2">
+            <div className="flex flex-col items-center gap-2">
+              <div className="flex w-[100px] h-[100px] overflow-hidden rounded-full bg-slate-400">
+                <Images
+                  src={user_profile}
+                  className="w-[200px] h-auto object-contain"
+                />
+              </div>
+              <div className="flex flex-col gap-1">
+                <span className="text-lg font-semibold">
+                  {user.nama_peserta}
+                </span>
+              </div>
+            </div>
+          </div> */}
+
+          <div className="flex flex-col justify-center items-center shadow-md border border-[#e7e7e7] rounded-2xl px-3 py-8 gap-3">
+            <div className="flex w-[100px] h-[100px] overflow-hidden rounded-full bg-slate-400">
+              <Images
+                src={user_profile}
+                className="w-[200px] h-auto object-contain"
+              />
+            </div>
+            <div className="flex flex-col text-center">
+              <span className="text-2xl font-semibold">
+                {user.nama_peserta}
+              </span>
+
+              <span className="text-lg">{user.email}</span>
+            </div>
+            <Button
+              type={"PrimaryButton"}
+              onClick={() => navigate("/dashboard-peserta")}
+              text={"Kembali"}
+              className="bg-[#1283b6] text-white  w-max text-start"
+            />
+          </div>
+          <div className="col-span-2 flex flex-col shadow-md border border-[#e7e7e7] rounded-2xl px-8 py-8 gap-6">
+            <h1 className="text-3xl font-bold">Profil Peserta</h1>
+
+            <div className="flex flex-col gap-2">
+              <div className="grid grid-cols-2 gap-8 text-lg">
+                <span className="text-lg">NIK/NIM</span>
+                {/*  <span>:</span> */}:{" "}
+                {user?.no_reg ? (
+                  user.no_reg
+                ) : (
+                  <span className="text-[#d4d4d4]">Belum diatur</span>
+                )}
+              </div>
+
+              <div className="grid grid-cols-2 gap-8 text-lg">
+                <span className="text-lg">No. hp</span>
+                {/*  <span>:</span> */}:{" "}
+                {user?.no_hp ? (
+                  user.no_hp
+                ) : (
+                  <span className="text-[#d4d4d4]">Belum diatur</span>
+                )}
+              </div>
+
+              <div className="grid grid-cols-2 gap-8 text-lg">
+                <span className="text-lg">Tgl. Lahir</span>
+                {/*  <span>:</span> */}:{" "}
+                {user?.tgl_lahir ? (
+                  user.tgl_lahir
+                ) : (
+                  <span className="text-[#d4d4d4]">Belum diatur</span>
+                )}
+              </div>
+
+              <div className="grid grid-cols-2 gap-8 text-lg">
+                <span className="text-lg">Jenis Kelamin</span>
+                {/*  <span>:</span> */}:{" "}
+                {user?.gender ? (
+                  user.gender
+                ) : (
+                  <span className="text-[#d4d4d4]">Belum diatur</span>
+                )}
+              </div>
+
+              <div className="grid grid-cols-2 gap-8 text-lg">
+                <span className="text-lg">Instansi</span>
+                {/* <span>:</span> */}
+                <span className={`text-lg `}>
+                  :{"  "}
+                  {user?.Instansi ? (
+                    user.Instansi
+                  ) : (
+                    <span className="text-[#d4d4d4]">Belum diatur</span>
+                  )}
+                </span>
+              </div>
+
+              <div className="grid grid-cols-2 gap-8 text-lg">
+                <span className="text-lg">Alamat</span>
+                {/* <span>:</span> */}
+                <span className="text-lg">
+                  :{"  "}
+                  {user?.Alamat ? (
+                    user.Alamat
+                  ) : (
+                    <span className="text-[#d4d4d4]">Belum diatur</span>
+                  )}
+                </span>
+              </div>
+            </div>
+          </div>
+
+          {/* <table className="font-semibold text-gray-800 mx-5">
+            <tr className="pt-4">
+              <td>
+                <span className="text-xl">NIK/NIM</span>
+              </td>
+              <td className="pl-2">
+                <span className="text-lg">: {user.no_reg}</span>
+              </td>
+            </tr>
+            <tr className="pt-8">
+              <td>
+                <span className="text-xl">Nama Peserta</span>
+              </td>
+              <td className="pl-2">
+                <span className="text-lg">: {user.nama_peserta}</span>
+              </td>
+            </tr>
+            <tr className="pt-4">
+              <td>
+                <span className="text-xl">Email</span>
+              </td>
+              <td className="pl-2">
+                <span className="text-lg">: {user.email}</span>
+              </td>
+            </tr>
+            <tr className="pt-4">
+              <td>
+                <span className="text-xl">No. HP</span>
+              </td>
+              <td className="pl-2">
+                <span className="text-lg">: {user.no_hp}</span>
+              </td>
+            </tr>
+            <tr className="pt-4">
+              <td>
+                <span className="text-xl">Tgl. Lahir</span>
+              </td>
+              <td className="pl-2">
+                <span className="text-lg">: {user.tgl_lahir}</span>
+              </td>
+            </tr>
+            <tr className="pt-4">
+              <td>
+                <span className="text-xl">Jenis Kelamin</span>
+              </td>
+              <td className="pl-2">
+                <span className="text-lg">: {user.gender}</span>
+              </td>
+            </tr>
+            <tr className="pt-4">
+              <td>
+                <span className="text-xl">Instansi</span>
+              </td>
+              <td className="pl-2">
+                <span className="text-lg">: {user.instansi}</span>
+              </td>
+            </tr>
+            <tr className="pt-4">
+              <td>
+                <span className="text-xl">Alamat</span>
+              </td>
+              <td className="pl-2">
+                <span className="text-lg">: {user.alamat}</span>
+              </td>
+            </tr>
+          </table> */}
+
+          {/* <Button
+            type={"PrimaryButton"}
+            onClick={() => navigate("/dashboard-peserta")}
+            text={"Kembali"}
+            className="bg-[#1283b6] text-white my-10 mx-5 w-max"
+          /> */}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProfilePeserta;
